24fc84633d09ebebea311908a935510936ef0c33
Rudra <117143791+MrFixy@users.noreply.github.com>
2023-01-25 17:41:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for your message! We will get back to you soon.");
+});
